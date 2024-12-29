9060b2d4e2a0029b7a7673eb20632f767b411c5f
Harish Suhirthan <164802325+SaiCoder108@users.noreply.github.com>
2024-12-28 23:47:53 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,60 @@
 /*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
 
+const sendEmail = (e) =>{
+    e.preventDefault()
 
+    emailjs.sendForm('service_96iosan','template_miwk8kk','#contact-form','pSM2liuDCbfu6WNLm')
+
+    .then(() =>{
+        // Show sent message
+        contactMessage.textContent = 'Message sent succcesfully ✅'
+
+        // Remove message after 5 seconds
+        setTimeout (() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear Input Fields
+        contactForm.reset()
+
+    }, () =>{
+        //Show Error Message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+contactForm.addEventListener('submit', sendEmail)
 /*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
